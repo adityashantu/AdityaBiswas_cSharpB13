dfff033e4e7aa4dcc8fb92fbc929129b4917d676
Aditya Biswas <roktimaditya@gmail.com>
2023-02-13 23:08:46 +0600

    Exam Done

---
+++ Exam2/Task3/RandomNumberGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class RandomNumberGenerator
+    {
+        private static int _seed;
+        private static Random _rand;
+
+/*        public static RandomNumberGenerator()
+        {
+            _seed = DateTime.Now.Microsecond;
+            _rand = new Random(_seed);
+        }*/   //static class can not have instances. I guess that's why this constructor is showing an error. 
+
+        public static int Generate()
+        {
+            return _rand.Next();
+        }
+    }
+}
---
+++ Exam2/Task4/Cannon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class Cannon : ICamera
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Cannon() { }
+
+        public Cannon(string model)
+        {
+            Model = model;
+        }
+
+        public Cannon(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+
+        public void TakePhoto(int width, int height)
+        {
+            Console.WriteLine($"Took photo of size: {width}x{height}");
+        }
+    }
+}
