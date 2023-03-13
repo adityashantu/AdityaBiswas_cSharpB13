45a43e91893f81ff02fbee31a41c0fc323145189
Aditya Biswas <roktimaditya@gmail.com>
2023-03-13 23:00:30 +0600

    Task 7 Done

--- src/Exam3/Task7/Program.cs
+++ src/Exam3/Task7/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+List<object> human = new List<object>();
+
+
+human.Add(new { Name = "Aditya", Age = 25 });
+human.Add(new { Name = "Zawad", Age = 28 });
+human.Add(new { Name = "Joyeeta", Age = 23 });
+human.Add(new { Name = "Trisha", Age = 22 });
+human.Add(new { Name = "Ovi", Age = 24 });
+human.Add(new { Name = "Tahu", Age = 21 });
+
+
+
+foreach (var person in human)
+{
+    Console.WriteLine("Name: {0}, Age: {1}", person.GetType().GetProperty("Name").GetValue(person, null), person.GetType().GetProperty("Age").GetValue(person, null));
+}
