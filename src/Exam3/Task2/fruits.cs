2271d1e06f947b954541c312b8678241e16d12e5
Aditya Biswas <roktimaditya@gmail.com>
2023-03-13 22:26:46 +0600

    Task 2 Done

---
+++ src/Exam3/Task2/fruits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    enum Fruits
+    {
+        Apple = 5,
+        Peach = 8,
+        Strawberries = 9,
+        Mango = 4,
+        Papaya = 6
+    }
+}
