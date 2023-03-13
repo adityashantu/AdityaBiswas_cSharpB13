4202ce119702c32f541f63f7cf755ad869e47529
Aditya Biswas <roktimaditya@gmail.com>
2023-03-13 22:17:51 +0600

    1st task done

---
+++ src/Exam3/Task1/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public struct Ticket
+    {
+        public int TicketNumber;
+        public int Price;
+
+        public Ticket(int tn, int p)
+        {
+            TicketNumber = tn;
+            Price = p;
+        }
+    }
+}
